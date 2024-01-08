4780fea39112f241cd8de7c37971f3a995448d4b
Atabek04 <otabek.shadimatov@gmail.com>
2024-01-08 14:05:11 +0600

    rewriting file adding index

--- example.go
+++ example.go
@@ -1,31 +1,70 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
+type JsonRequest struct {
+	Message string `json:"message"`
+}
+
+type JsonResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 func main() {
-	r := gin.Default()
+	http.HandleFunc("/", handlePostRequest)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("."))))
+	fmt.Println("Server is listening on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
 
-	r.POST("", func(c *gin.Context) {
-		var json struct {
-			Message string `json:"message"`
-		}
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet && r.URL.Path == "/" {
+		http.ServeFile(w, r, "index.html")
+		return
+	}
 
-		if err := c.BindJSON(&json); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	if r.Method != http.MethodPost {
+		sendJsonResponse(w, http.StatusMethodNotAllowed, "Method not allowed")
+		return
+	}
 
-		response := gin.H{
-			"status": http.StatusOK,
-			"data":   json.Message,
-		}
+	var requestData map[string]interface{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&requestData)
+	if err != nil {
+		sendJsonResponse(w, http.StatusBadRequest, "Invalid JSON format")
+		return
+	}
 
-		c.JSON(http.StatusOK, response)
-	})
+	if message, ok := requestData["message"].(string); !ok || message == "" {
+		sendJsonResponse(w, http.StatusBadRequest, "Invalid JSON message")
+		return
+	}
 
-	r.Run()
+	fmt.Println("Received message:", requestData["message"])
+
+	response := JsonResponse{
+		Status:  "success",
+		Message: "Data successfully received",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func sendJsonResponse(w http.ResponseWriter, statusCode int, message string) {
+	response := JsonResponse{
+		Status:  fmt.Sprintf("%d", statusCode),
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response)
 }
